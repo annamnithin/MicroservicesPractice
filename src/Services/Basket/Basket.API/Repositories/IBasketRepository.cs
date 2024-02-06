dae0ce64a3d1413f137e2bbdb77b99a9264cd81e
annamnithin <nithinsiva71@gmail.com>
2024-02-05 23:18:04 -0600

    Added Catalog API and added Basket Repository and Basket Entity.

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using Basket.API.Entities;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateCart(ShoppingCart basket);
+        Task DeleteCart(string userName);
+    }
+}
